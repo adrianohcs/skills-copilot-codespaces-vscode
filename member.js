134c5523019a347764b6cd4e18cd70d1ea3d77ab
Adriano Silva <86486880+adrianohcs@users.noreply.github.com>
2025-02-24 15:10:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  const skills = [
+    {
+      name: 'HTML',
+      value: 90
+    },
+    {
+      name: 'CSS',
+      value: 90
+    },
+    {
+      name: 'JavaScript',
+      value: 80
+    },
+    {
+      name: 'React',
+      value: 70
+    }
+  ];
+
+  return skills;
+}
